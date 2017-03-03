87678871b5065b241c8d94565d79edd602d06fa4
Daniel <danielems3@outlook.com>
2017-03-02 20:23:49 -0500

    build a treasure class

--- dungeon_dudes.py
+++ dungeon_dudes.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+
+class Treasure:
+    
+    def __init__(self, title):
+        self.title = title
+
+    def __str__ (self):
+        view = "{0}"
+        return view.format(self.title)
+
 def main():
 
-    print("Hello World")
+    treasure = Treasure("Fresh Pair of Undies")
+
+    print(str(treasure))
+
+    
 
 if __name__ == "__main__":
     main()
